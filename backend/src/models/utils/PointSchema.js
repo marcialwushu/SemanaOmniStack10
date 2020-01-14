3d81fff8a28d7d20720fa511048f12004b46bfa4
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-14 17:16:52 -0200

    location lat:long add

---
+++ backend/src/models/utils/PointSchema.js
@@ -0,0 +1,15 @@
+const mongooose = require('mongoose');
+
+const PointSchema = new mongooose.Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+    },
+    coordinates: {
+        type: [Number],
+        required: true,
+    },
+});
+
+module.exports = PointSchema;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,34 @@
 const { Router } = require('express');
 const axios = require('axios');
+const Dev = require('./models/dev');
 
 const routes = Router();
 
 
 routes.post('/devs', async (req,res) => {
-    const { github_username } = req.body;
+    const { github_username, techs, latitude, longitude } = req.body;
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
     const { name = login, avatar_url, bio } = apiResponse.data;
 
-    console.log(name, avatar_url, bio, github_username);
-     
-    return res.json({ message: 'Hello OmniStack' });
+    const techArray = techs.split(',').map(tech => tech.trim());
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    }
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techArray,
+        location
+    })
+        
+    return res.json(dev);
 });
 
 

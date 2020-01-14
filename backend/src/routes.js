47f3613d8ded760df9e6b8bc544c614041c13980
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-14 15:53:29 -0200

    DevSchema model add

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const routes = Router();
 
 
-routes.post('/users', (req,res) => {
+routes.post('/devs', (req,res) => {
     console.log(req.body);
     return res.json({ message: 'Hello OmniStack' });
 });

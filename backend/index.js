fd679a949377a138d5c63f1ae8159d473758a227
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-14 14:17:01 -0200

    isomnia tool test

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    return res.send('Hello World');
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE 
+
+// Tipos de parâmetros :
+// Query Params: req.query (Filtros, ordenação,paginação)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body : request.body (Dados para criação ou alteração de um registro)
+
+app.post('/users', (req,res) => {
+    console.log(req.body);
+    return res.json({ message: 'Hello OmniStack' });
 });
 
 app.listen(3333);

313a25d19bb2691f39caf75ccfd35460c88dc13e
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-14 16:31:27 -0200

    github api connect

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
 
-routes.post('/devs', (req,res) => {
-    console.log(req.body);
+routes.post('/devs', async (req,res) => {
+    const { github_username } = req.body;
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = apiResponse.data;
+
+    console.log(name, avatar_url, bio, github_username);
+     
     return res.json({ message: 'Hello OmniStack' });
 });
 
 
-module.exports = routes;
+module.exports = routes; 

4ee5ffd9a46a6ebfc8a5de798e78489c2c054e39
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-14 17:41:21 -0200

    DevController create and find

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,46 @@
+
+const axios = require('axios');
+const Dev = require('../models/dev');
+
+module.exports = {
+    async index(req, res) {
+        const devs = await Dev.find();
+
+        return res.json(devs);
+    },
+
+    async store( req,res) {
+        const { github_username, techs, latitude, longitude } = req.body;
+
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+        const { name = login, avatar_url, bio } = apiResponse.data;
+    
+        const techArray = techs.split(',').map(tech => tech.trim());
+    
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        }
+    
+        dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techArray,
+            location
+        })
+
+        }
+    
+        
+            
+        return res.json(dev);
+    }
+    
+};

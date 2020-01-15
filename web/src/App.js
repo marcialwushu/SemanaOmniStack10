a9ca1444f2b5113492179202459d1fbc068bce41
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-15 16:18:21 -0200

    screen Cadastro init layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './global.css'
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
 
 
 function App() {
@@ -7,7 +10,34 @@ function App() {
 
   return (
     <div id="app">
-      <aside></aside>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+          
+        <div class="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input name="techs" id="techs" required></input>
+        </div>
+          
+         <div className="input-group">
+         <div class="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input name="latitude" id="latitude" required></input>
+        </div>
+
+        <div class="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input name="longitude" id="longitude" required></input>
+        </div>
+         </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
       <main></main>
     </div>
   );

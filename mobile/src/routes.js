228f83fa2088e7c206bc93b2497aa7939724b0a4
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-01-20 20:13:05 -0200

    yarn add react-native-webview

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -22,6 +22,7 @@ const Routes = createAppContainer(
     }, {
         defaultNavigationOptions: {
             headerTintColor: '#fff',
+            headerBackTitleVisible: false,
             headerStyle: {
                 backgroundColor: '#7D40E7'
             }
